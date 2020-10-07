ca49e6717f36e65d08072ac97fa0a371dcfd267d
liucu0135 <darkcoke2005@163.com>
2020-10-07 13:33:52 +0800

    result write to csv

--- Run_mo.py
+++ Run_mo.py
@@ -4,6 +4,7 @@ from Simulator import Simulator as Sim
 import copy
 import pickle
 import pandas as pd
+import numpy as np
 import time
 
 # vvr_rep10, vvr%10:
@@ -40,7 +41,7 @@ ccs=[]
 cc=[]
 data=[noc,nom,nol,ll,nof]
 sets=[]
-# data=[]
+data=[]
 ts=[]
 tds=[]
 times=[]
@@ -49,13 +50,14 @@ columns=['colors','models','lanes','lane length', 'filling','color change','time
 
 # for set in range(6):
 set=3
-pres=[0.2, 0.4, 0.5, 0.6, 0.8]
+pres=np.arange(100)/100
+# pres=[0.2, 0.4, 0.5, 0.6, 0.8]
 for pref in pres:
     preFill = nol[set]*ll[set]*nof[set] // 10
     st = VVR_Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, preference=pref,cc_file='./csv_files/cost.csv')
     s = Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, VVR_temp=st,repeat=10, preference=pref,cc_file='./csv_files/cost.csv', color_dist_file='./csv_files/total_orders.csv')
 
-    repeat_epoches=20
+    repeat_epoches=1
     # print(s.mc_tab)
     # print(s.bank.get_view_state())
     # print(s.bank.front_hist())
@@ -67,30 +69,27 @@ for pref in pres:
     for epoch in range(repeat_epoches):
         s.reset()
         start_time = time.time()
+        td_epo=0
         for i in range(300-preFill-1):
             # if i%10==0:
             #     print(i, ' out of 1000 finihsed!     ', epoch, ' out of 10 epcoh', td_sum)
             s.BBA_rule_step_in()
-            if s.VVR_rule_out(i%1==0):
+            if s.VVR_rule_out(i%10==1):
                 td=s.get_distortion(absolute=True, tollerance=0)/10
-                td_sum+=td
+                td_epo+=td
             else:
                 print("release failed")
-        times.append(time.time() - start_time)
-        cc.append(s.cc)
-        tds.append(td)
-        sets.append(set)
+        # times.append(time.time() - start_time)
+        # cc.append(s.cc)
+        # tds.append(td_epo)
         cc_sum += s.cc
+        td_sum += td_epo
         t_sum += time.time() - start_time
-    ccs.append(cc_sum/repeat_epoches)
-    ts.append(t_sum/repeat_epoches)
-    tds.append(t_sum/repeat_epoches)
-
+    # ccs.append(cc_sum/repeat_epoches)
+    # ts.append(t_sum/repeat_epoches)
+    # tds.append(t_sum/repeat_epoches)
+        data.append([cc_sum/repeat_epoches, td_sum/repeat_epoches, t_sum/repeat_epoches])
     print("color-modle:{}-{}, bank: {}X{}, cc:{}, td:{}".format(noc[set],nom[set],nol[set],ll[set],cc_sum/repeat_epoches,td_sum/repeat_epoches))
-    data.append(ccs)
-    data.append(ts)
-    data.append(pres)
-    data.append(cc)
-    data.append(times)
+
 result=pd.DataFrame.from_records(data)
-result.to_csv('result_mo_ccm_mod1.csv')
+result.to_csv('bench.csv')
