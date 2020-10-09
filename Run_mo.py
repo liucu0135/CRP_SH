c30ab2aa5b58c669250412cee5b0ff5885a5d0a6
liucu0135 <darkcoke2005@163.com>
2020-10-10 00:05:50 +0800

    1000 orders nc

--- Run_mo.py
+++ Run_mo.py
@@ -54,8 +54,10 @@ pres=np.arange(100)/100
 # pres=[0.2, 0.4, 0.5, 0.6, 0.8]
 for pref in pres:
     preFill = nol[set]*ll[set]*nof[set] // 10
-    st = VVR_Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, preference=pref,cc_file='./csv_files/cost.csv')
-    s = Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, VVR_temp=st,repeat=100, preference=pref,cc_file='./csv_files/cost.csv', color_dist_file='./csv_files/total_orders.csv')
+    st = VVR_Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, preference=pref,cc_file=None)
+    # st = VVR_Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, preference=pref,cc_file='./csv_files/cost.csv')
+    s = Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, VVR_temp=st,repeat=100, preference=pref,cc_file=None, color_dist_file=None)
+    # s = Sim(num_color=noc[set], num_model=nom[set], num_lanes=nol[set], lane_length=ll[set], capacity=preFill, VVR_temp=st,repeat=100, preference=pref,cc_file='./csv_files/cost.csv', color_dist_file='./csv_files/total_orders.csv')
 
     repeat_epoches=1
     # print(s.mc_tab)
@@ -92,4 +94,4 @@ for pref in pres:
     print("color-modle:{}-{}, bank: {}X{}, cc:{}, td:{}".format(noc[set],nom[set],nol[set],ll[set],cc_sum/repeat_epoches,td_sum/repeat_epoches))
 
 result=pd.DataFrame.from_records(data)
-result.to_csv('bench_cc.csv')
+result.to_csv('bench_nc.csv')
